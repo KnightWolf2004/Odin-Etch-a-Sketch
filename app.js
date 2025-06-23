1c3f81da990dc17391de29d76ba27ddf99a0b988
KnightWolf2004 <22uec018@lnmiit.ac.in>
2025-06-23 18:35:01 +0530

    Addeed a 'clear' button that refershes the canvas. Also added a class 'visited' to all divs that are colored to help with the feature.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const workspace = document.querySelector(".workspace");
+const button = document.querySelector(".submit");
+const input = document.querySelector("input");
+const clear = document.querySelector(".clear");
 
 function makenewcanvas(size = 16) {
     workspace.replaceChildren();
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        row.classList.add("canvas");
+        // row.classList.add("canvas");
         for (let j = 0; j < size; j++) {
             const col = document.createElement("div");
             col.classList.add("column");
-            col.classList.add("canvas");
+            // col.classList.add("canvas");
             col.id = [String(i) + ' ' + String(j)];
             row.appendChild(col);
         }
@@ -25,6 +28,7 @@ workspace.addEventListener("mouseover", (event) => {
         // console.log(block.style.cssText.length);
         if (block.style.cssText.length === 0) {
             block.setAttribute("style", `background: rgba(${Math.random() * (256 - 0) + 0},${Math.random() * (256 - 0) + 0},${Math.random() * (256 - 0) + 0}); opacity:0.1`);
+            block.classList.add("visited");
         } else {
             // console.log(block.style.opacity);
             const opacityval = Number(block.style.opacity);
@@ -35,8 +39,6 @@ workspace.addEventListener("mouseover", (event) => {
     }
 });
 
-const button = document.querySelector("button");
-const input = document.querySelector("input");
 
 input.addEventListener("input", () => {
     let msg = document.querySelector(".invalid-input");
@@ -56,5 +58,16 @@ button.addEventListener("click", () => {
         msg.textContent = "Please enter a valid number between 4 and 100";
     }
 });
+
+clear.addEventListener("click", () => {
+    const children = document.querySelectorAll(".visited");
+    // console.log(children);
+    children.forEach(ele => {
+        ele.classList.remove("visited");
+        // console.log(ele.style.cssText);
+        ele.style.cssText = "";
+    })
+})
+
 
 makenewcanvas();
