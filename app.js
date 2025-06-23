7271fb7349a2f18104a270435367997926e401c7
KnightWolf2004 <22uec018@lnmiit.ac.in>
2025-06-23 16:37:06 +0530

    Divs change color when mouse enters a div. Also made it so that the color is randomized and implemented a progressive darkening effect with opacity increasing by 0.1 with each interaction.

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ for (let i = 0; i < 16; i++) {
         const col = document.createElement("div");
         col.classList.add("column");
         col.classList.add("canvas");
+        col.id = [String(i) + ' ' + String(j)];
         row.appendChild(col);
     }
     workspace.appendChild(row);
 }
+
+workspace.addEventListener("mouseover", (event) => {
+    let target = event.target.id;
+    // console.log(target);
+    if (target.length > 0) {
+        const block = document.getElementById(target);
+        // console.log(block.style.cssText.length);
+        if (block.style.cssText.length === 0) {
+            block.setAttribute("style", `background: rgba(${Math.random() * (256 - 0) + 0},${Math.random() * (256 - 0) + 0},${Math.random() * (256 - 0) + 0}); opacity:0.1`);
+        } else {
+            // console.log(block.style.opacity);
+            const opacityval = Number(block.style.opacity);
+            if (opacityval < 1) {
+                block.style.opacity = opacityval + 0.1;
+            }
+        }
+    }
+});
